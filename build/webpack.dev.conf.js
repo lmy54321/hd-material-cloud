08c7c4f125ba695cc4fc689caea8daa886d88fc1
lmy54321 <1147038932@qq.com>
2019-06-17 18:30:46 +0800

    2019-6-14 Signed-off-by: lmy54321 <1147038932@qq.com>
    Signed-off-by: lmy54321 <1147038932@qq.com>

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -69,36 +69,36 @@ const devWebpackConfig = merge(baseWebpackConfig, {
 })
 
 // express 配置server
-// let apiServer = express()
-// let bodyParser = require('body-parser')
-// apiServer.use(bodyParser.urlencoded({ extended: true }))
-// apiServer.use(bodyParser.json())
-// let apiRouter = express.Router()
-// let fs = require('fs')
-// apiRouter.route('/:apiName') //接口路径
-//   .all(function (req, res) {
-//     fs.readFile('./db.json', 'utf8', function (err, data) {  //读取接口文件
-//       if (err) throw err
-//       var data = JSON.parse(data)
-//       if (data[req.params.apiName]) {
-//         res.json(data[req.params.apiName])
-//       }
-//       else {
-//         res.send('no such api name')
-//       }
-//
-//     })
-//   })
-//
-//
-// apiServer.use('/api', apiRouter);
-// apiServer.listen(3000, function (err) {
-//   if (err) {
-//     console.log(err)
-//     return
-//   }
-//   console.log('Listening at http://localhost:' + 3000 + '\n')
-// })
+let apiServer = express()
+let bodyParser = require('body-parser')
+apiServer.use(bodyParser.urlencoded({ extended: true }))
+apiServer.use(bodyParser.json())
+let apiRouter = express.Router()
+let fs = require('fs')
+apiRouter.route('/:apiName') //接口路径
+  .all(function (req, res) {
+    fs.readFile('./db.json', 'utf8', function (err, data) {  //读取接口文件
+      if (err) throw err
+      var data = JSON.parse(data)
+      if (data[req.params.apiName]) {
+        res.json(data[req.params.apiName])
+      }
+      else {
+        res.send('no such api name')
+      }
+
+    })
+  })
+
+
+apiServer.use('/api', apiRouter);
+apiServer.listen(3000, function (err) {
+  if (err) {
+    console.log(err)
+    return
+  }
+  console.log('Listening at http://localhost:' + 3000 + '\n')
+})
 
 module.exports = new Promise((resolve, reject) => {
   portfinder.basePort = process.env.PORT || config.dev.port
